861ea127a4c1b7960f886a6e5b12c8d422660f55
vishal kumar <vishalkumar150520@gmail.com>
2023-03-20 13:34:51 +0530

    project

---
+++ registration.php
@@ -0,0 +1,10 @@
+<?php
+/** Custom Menu registration php
+ * @category  Bluethink
+ * @author    Ritik
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Bluethink_CustomMenu',
+    __DIR__
+);
